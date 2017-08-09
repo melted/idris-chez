e567385167eb0db5a0a2bea6dd4cb5497b89e2d8
Niklas Larsson <niklas@mm.st>
2017-08-09 22:11:27 +0200

    Implement Prelude.File handling

--- rts/rts.ss
+++ rts/rts.ss
@@ -12,4 +12,15 @@
           (x (max 0 len))
           (end (min l (+ b x))))
             (substring s b end)))
-    
+
+
+;; TODO: Add error handling.
+(define (idris-chez-fileopen file mode)
+        (case mode 
+            ("r") (open-file-input-port file (file-options 'no-create))
+            ("wx") (open-file-output-port file)
+            ("a") (open-file-output-port file (file-options 'no-fail 'no-truncate))
+            ("r+") (open-file-input/output-port file (file-options 'no-create))
+            ("w+") (open-file-input/output-port file (file-options 'no-fail))
+            ("w+x") (open-file-input/output-port file)
+            ("a+") (open-file-input/output-port file (file-options 'no-fail 'no-truncate)))))
--- src/Chez/Compatibility.hs
+++ src/Chez/Compatibility.hs
@@ -24,11 +24,40 @@ import Chez.Util
 -- Change the sdecls, this is used when we need
 -- to be aware of the context.
 fixup :: [(Name, SDecl)] -> [(Name, SDecl)]
-fixup decls = decls
+fixup decls = map rewriteDecl decls
 
--- Check a foreign call
+-- Check a foreign call, and return substitute code if a match
 intercept :: FDesc -> FDesc -> [(FDesc, LVar)] -> Maybe String
--- isNull used from from Prelude.Strings
+-- isNull used from Prelude.Strings
 intercept _ (FStr "isNull") [(_, p)] = Just $ predicate (call "=" ["0", compileVar p])
+-- Prelude.File
+intercept _ (FStr "fileOpen") [(_, _), (_, file), (_, mode)] = 
+                            Just $ call "idris-chez-fileopen" [compileVar file, compileVar mode]
+intercept _ (FStr "fileSize") [(_, _), (_, p)] = Just $ call "file-length" [compileVar p]
+-- TODO: showerror and mkFileError should be better
+intercept _ (FStr "idris_showerror") [(_, _), (_, i)] = Just $ sstr "Unhelpful error"
+intercept _ (FStr "idris_mkFileError") [(_, _), (_, p)] = Just $ sexp ["0", "0"]
+
 intercept _ _ _ = Nothing
 
+rewriteDecl :: (Name, SDecl) -> (Name, SDecl)
+rewriteDecl (n, sd) | n == sUN "Prelude.File.fgetc" = (n, rewriteFgetc sd)
+rewriteDecl d = d
+
+-- Yes, this machinery basically exists so we can rewrite the fgetc FFI
+-- descriptor to contain another, less common, name.
+-- Oh, how I wish SExpr had derived Generic.
+rewriteFgetc sd = rewriteFFI "fgetc" "idris_fgetc" sd
+
+rewriteFFI :: String -> String -> SDecl -> SDecl
+rewriteFFI from to (SFun a b c exp) = SFun a b c (rewriteExp exp)
+    where
+        rewriteExp (SForeign r (FStr from) a) = SForeign r (FStr to) a
+        rewriteExp (SLet v exp body) = SLet v (rewriteExp exp) (rewriteExp body)
+        rewriteExp (SCase a b alts) = SCase a b (map rewriteAlt alts)
+        rewriteExp (SChkCase a alts) = SChkCase a (map rewriteAlt alts)
+        rewriteExp e = e
+
+        rewriteAlt (SConCase a b c d exp) = SConCase a b c d (rewriteExp exp)
+        rewriteAlt (SConstCase a exp) = SConstCase a (rewriteExp exp)
+        rewriteAlt (SDefaultCase exp) = SDefaultCase (rewriteExp exp)
